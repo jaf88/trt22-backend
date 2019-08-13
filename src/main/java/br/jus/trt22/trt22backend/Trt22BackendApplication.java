1674061f29e5c6049ef6407d1c8f82ffeb4d0242
Joaquim Alves França <joaquim.franca@TRT22DC.trt22.jus.br>
2019-08-13 14:16:44 -0300

    first commit

---
+++ src/main/java/br/jus/trt22/trt22backend/Trt22BackendApplication.java
@@ -0,0 +1,13 @@
+package br.jus.trt22.trt22backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Trt22BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Trt22BackendApplication.class, args);
+	}
+
+}
